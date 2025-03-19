82f449a684ed31154127f024fd0a2b9e5db98128
haziq812 <haziq.zainuddin@usaha-kreatif.com>
2025-03-20 06:01:03 +0800

    commit socket io setup

---
+++ src/context/SocketContext.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+// Connect to WebSocket server
+const SOCKET_SERVER_URL = "http://localhost:3001";
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null);
+    
+    useEffect(() => {
+        const newSocket = io(SOCKET_SERVER_URL);
+        setSocket(newSocket);
+
+        newSocket.on("connect", () => {
+            console.log("Connected to WebSocket:", newSocket.id);
+        });
+
+        newSocket.on("message", (data) => {
+            console.log("Received message:", data);
+        });
+
+        return () => newSocket.close();
+    }, []);
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export const useSocket = () => useContext(SocketContext);
--- src/index.js
+++ src/index.js
@@ -6,20 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'; 
 import { AuthProvider } from './context/AuthContext';
 import { FriendsProvider } from './context/FriendsContext';
+import { SocketProvider } from "./context/SocketContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>  
-    <AuthProvider>
-      <FriendsProvider>
-        <App />
-      </FriendsProvider>
-    </AuthProvider>
-
-    </BrowserRouter>,
-  </React.StrictMode>
-);
+  <SocketProvider>
+    <React.StrictMode>
+      <BrowserRouter>  
+      <AuthProvider>
+        <FriendsProvider>
+          <App />
+        </FriendsProvider>
+      </AuthProvider>
+      </BrowserRouter>,
+    </React.StrictMode>
+  </SocketProvider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
